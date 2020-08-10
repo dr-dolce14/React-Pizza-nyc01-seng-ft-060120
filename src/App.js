919079da5c0627261a8431004bb187a58e371e10
dr-dolce14 <dolcemascop1@gmail.com>
2020-08-10 11:22:29 -0400

    Done.

--- src/App.js
+++ src/App.js
@@ -2,13 +2,66 @@ import React, { Component, Fragment } from 'react';
 import Header from './components/Header'
 import PizzaForm from './components/PizzaForm'
 import PizzaList from './containers/PizzaList'
+
 class App extends Component {
+
+  state = {
+    pizzaArray: [],
+    pizza: {}
+  }
+
+
+  componentDidMount() {
+    fetch("http://localhost:3000/pizzas")
+    .then(resp => resp.json())
+    .then(pizzaData => {
+      this.setState({
+        pizzaArray: pizzaData
+      })
+    })
+  }
+
+  editHandler = (obj) => {
+    this.setState({
+      pizza: obj
+    })
+  }
+
+  submitHandler = (obj, id) => {
+    fetch(`http://localhost:3000/pizzas/${id}`, {
+      method: "PATCH",
+      headers: {
+        'accepts': 'application/json',
+        'content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        topping: obj.topping,
+        size: obj.size,
+        vegetarian: obj.vegetarian
+      })
+    })
+    .then(resp => resp.json())
+    // .then(pizzaObj => console.log(pizzaObj))
+    .then(pizzaObj => {this.editPizzas(pizzaObj)
+
+    })
+  }
+
+  editPizzas = (pizzaObj) => {
+   let index = Number(pizzaObj.id) - 1
+   this.state.pizzaArray.splice(index, 1, pizzaObj)
+   this.setState({
+     pizzaArray: this.state.pizzaArray
+   })
+  }
+
+
   render() {
     return (
       <Fragment>
         <Header/>
-        <PizzaForm/>
-        <PizzaList/>
+        <PizzaForm pizza={this.state.pizza} submitHandler={this.submitHandler}/>
+        <PizzaList pizzas={this.state.pizzaArray} editHandler={this.editHandler}/>
       </Fragment>
     );
   }
--- src/containers/PizzaList.js
+++ src/containers/PizzaList.js
@@ -1,8 +1,12 @@
 import React, { Component } from 'react';
 import Pizza from '../components/Pizza'
 class PizzaList extends Component {
+  
 
   render() {
+
+    
+
     return (
       <table className="table table-striped">
         <thead>
@@ -15,7 +19,7 @@ class PizzaList extends Component {
         </thead>
         <tbody>
           {
-            //render Pizza here
+            this.props.pizzas.map(pizza => <Pizza key={pizza.id} pizza={pizza} editHandler={this.props.editHandler}/>)
           }
         </tbody>
       </table>
